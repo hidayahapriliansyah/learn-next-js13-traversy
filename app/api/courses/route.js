e205b18c28cf63c11304f62ec3a8d00bf15034c5
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-05-12 13:27:39 +0700

    getting-body-data

--- app/api/courses/route.js
+++ app/api/courses/route.js
@@ -1,6 +1,21 @@
 import { NextResponse } from 'next/server';
 import courses from './data.json';
+import { v4 as uuiv4 } from 'uuid';
 
 export async function GET(request) {
+  return NextResponse.json(courses);
+};
+
+export async function POST(request) {
+  const { title, description, level, link } = await request.json();
+  const newCourse = {
+    id: uuiv4(),
+    title,
+    description,
+    level,
+    link,
+  };
+  courses.push(newCourse);
+
   return NextResponse.json(courses);
 };
